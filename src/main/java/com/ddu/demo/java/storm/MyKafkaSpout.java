1e0d4f2e2aac25e145724fb670c017f1478d1690
wxl24life <wxl24life@gmail.com>
2019-07-19 10:04:09 +0800

    do refactoring: remove while(true) in nextTuple()

--- src/main/java/com/ddu/demo/java/storm/MyKafkaSpout.java
+++ src/main/java/com/ddu/demo/java/storm/MyKafkaSpout.java
@@ -46,45 +46,41 @@ class MyKafkaSpout<K, V> extends BaseRichSpout {
     @Override
     public void nextTuple() {
         try {
-            while (true) {
-                // 使用 poll 方法持续轮询，参数代表了超时时间（单位 ms），在 consumer 缓冲区里没有可用数据时会发生阻塞，
-                // 超时参数用于控制阻塞的时间，指定了方法在多久之后可以返回，不管有无数据都返回
-                // 超时时间的设定取决于应用程序对响应速度的要求，比如在多长时间内把控制权交给轮询线程
-                // 如果设置成 poll(0)，不阻塞，直接返回
+            // 使用 poll 方法持续轮询，参数代表了超时时间（单位 ms），在 consumer 缓冲区里没有可用数据时会发生阻塞，
+            // 超时参数用于控制阻塞的时间，指定了方法在多久之后可以返回，不管有无数据都返回
+            // 超时时间的设定取决于应用程序对响应速度的要求，比如在多长时间内把控制权交给轮询线程
+            // 如果设置成 poll(0)，不阻塞，直接返回
 
-                // 关于 kafka consumer 轮询的进一步说明
-                // 1. 轮询除了获取分区数据，在首次调用新消费者的 poll() 方法时，会负责查找 GroupCoordinator，然后加入 group，并接受分配过来的分区
-                // 2. 如果发生 rebalance，整个过程也是在轮询期间完成的
-                // 3. 心跳也是从轮询里发送出去的
-                // 所以，要确保在轮训期间所做的任何处理工作尽快完成
-                if (waitingToEmitted.isEmpty()) {
-                    ConsumerRecords<K, V> records = consumer.poll(100);
+            // 关于 kafka consumer 轮询的进一步说明
+            // 1. 轮询除了获取分区数据，在首次调用新消费者的 poll() 方法时，会负责查找 GroupCoordinator，然后加入 group，并接受分配过来的分区
+            // 2. 如果发生 rebalance，整个过程也是在轮询期间完成的
+            // 3. 心跳也是从轮询里发送出去的
+            // 所以，要确保在轮训期间所做的任何处理工作尽快完成
+            if (waitingToEmitted.isEmpty()) {
+                ConsumerRecords<K, V> records = consumer.poll(100);
 
-                    /*Set<TopicPartition> topicPartitions = records.partitions();
-                    for (TopicPartition tp : topicPartitions) {
-                        tp.partition(), tp.topic();
-                    }*/
-                    for (ConsumerRecord<K, V> record : records) {
-                        TopicPartition topicPartition = new TopicPartition(record.topic(), record.partition());
-                        if (waitingToEmitted.containsKey(topicPartition)) {
-                            waitingToEmitted.get(topicPartition).add(record);
-                        } else {
-                            // List<ConsumerRecord<K, V>> list = new ArrayList<>();
-                            // list.add(record);
-                            waitingToEmitted.put(topicPartition, new ArrayList<>(Collections.singletonList(record)));
-                        }
+                /*Set<TopicPartition> topicPartitions = records.partitions();
+                for (TopicPartition tp : topicPartitions) {
+                    tp.partition(), tp.topic();
+                }*/
+                for (ConsumerRecord<K, V> record : records) {
+                    TopicPartition topicPartition = new TopicPartition(record.topic(), record.partition());
+                    if (waitingToEmitted.containsKey(topicPartition)) {
+                        waitingToEmitted.get(topicPartition).add(record);
+                    } else {
+                        // List<ConsumerRecord<K, V>> list = new ArrayList<>();
+                        // list.add(record);
+                        waitingToEmitted.put(topicPartition, new ArrayList<>(Collections.singletonList(record)));
                     }
-                } else {
-                    emitIfWaitingNotEmmited();
                 }
-                // consumer.commitAsync(); // 异步提交，碰到可恢复异常时也不会重试
             }
+
+            emitIfWaitingNotEmmited();
+
+            // consumer.commitAsync(); // 异步提交，碰到可恢复异常时也不会重试
         } catch (Exception e) {
             // log error
             e.printStackTrace();
-        } finally {
-            // 退出关闭 consumer，随之关闭网络连接和 socket，并会立即触发一次再均衡
-            consumer.close();
         }
     }
 
@@ -98,10 +94,12 @@ class MyKafkaSpout<K, V> extends BaseRichSpout {
                 this.collector.emit(Arrays.asList(consumerRecord.topic(), consumerRecord.partition(), consumerRecord.offset(),
                         consumerRecord.key(), consumerRecord.value()));
                 this.consumer.commitSync();
-                break;
             }
+
             // remove 跟 hasNext 方法一起使用
-            waitingToEmitIter.remove();
+            if (waitingToEmitForTp.isEmpty()) {
+                waitingToEmitIter.remove();
+            }
         }
     }
 
@@ -109,4 +107,11 @@ class MyKafkaSpout<K, V> extends BaseRichSpout {
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
         outputFieldsDeclarer.declare(new Fields("topic", "partition", "offset", "key", "value"));
     }
+
+    @Override
+    public void close() {
+        // 退出关闭 consumer，随之关闭网络连接和 socket，并会立即触发一次再均衡
+        this.consumer.close();
+    }
+
 }
