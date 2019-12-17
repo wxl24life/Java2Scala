0d8e32b8b67d55fd382e1ef747ffadecedcde36b
wxl24life <wxl24life@gmail.com>
2019-12-17 09:13:53 +0800

    code refactoring

--- src/main/java/com/ddu/demo/java/algorithm/leetcode/LC200NumberOfIslands.java
+++ src/main/java/com/ddu/demo/java/algorithm/leetcode/LC200NumberOfIslands.java
@@ -10,6 +10,10 @@ public class LC200NumberOfIslands {
     static int[][] directions = new int[][] {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     // 8:36
     public static int numIslands(int[][] grid) {
+        // edge cases
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+
+        // rows & columns
         int R = grid.length;
         int C = grid[0].length;
 
@@ -17,8 +21,6 @@ public class LC200NumberOfIslands {
         for (int i = 0; i < R; ++i) {
             for (int j = 0; j < C; ++j) {
                 if (grid[i][j] == 1) {
-                    // mark grid[i][j] as visited
-                    grid[i][j] = 0;
                     helper(grid, R, C, i, j);
                     ans++;
                 }
@@ -31,16 +33,15 @@ public class LC200NumberOfIslands {
     // dfs search,
     // mark all grid[i,j] to 0 if it it is connected with grid[r0][c0]
     private static void helper(int[][] grid, int R, int C, int r0, int c0) {
-        for (int[] dir : directions) {
-            int r = r0 + dir[0];
-            int c = c0 + dir[1];
-            if (r >= R || r < 0 || c >= C || c < 0 || grid[r][c] == 0) {
-                continue;
-            }
+        if (r0 < R && r0 >= 0 && c0 < C && c0 >= 0 && grid[r0][c0] == 1) {
             // mark as visited
-            grid[r][c] = 0;
-            // dfs search
-            helper(grid, R, C, r, c);
+            grid[r0][c0] = 0;
+            for (int[] dir : directions) {
+                int r = r0 + dir[0];
+                int c = c0 + dir[1];
+                // dfs search
+                helper(grid, R, C, r, c);
+            }
         }
     }
 
